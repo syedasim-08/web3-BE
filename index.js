5028459f3ae4ebee110a2637ccc66c2ecfe69d77
syedasim-08 <syedasim@webllisto.com>
2024-09-27 19:57:16 +0530

    fix:cors-4-fixedcors

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ async function main() {
 }
 
 console.log('Allowed Origin:', process.env.FRONTEND_URL);
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-  next();
+server.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Or specific origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.sendStatus(204);
 });
 
 // Define allowed origins
@@ -30,24 +32,21 @@ const allowedOrigins = [
     "https://openocean-nft.vercel.app/art"
 ];
 
-// CORS options
 const corsOptions = {
-    origin: (origin, callback) => {
-      console.log('Incoming Origin:', origin); // Log the incoming origin
-        // Allow requests with no origin (like Postman)
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST'], // Specify allowed methods
-    allowedHeaders: ['Content-Type'], // Specify allowed headers
-  
+  origin: (origin, callback) => {
+      console.log('Incoming Origin:', origin);
+      if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
 };
 
-// Use CORS middleware with the specified options
 server.use(cors(corsOptions));
+
 
 
 //bodyParser
